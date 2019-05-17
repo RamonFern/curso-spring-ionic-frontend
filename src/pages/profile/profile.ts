a8a51e2f6f69db75310736871db3a475cc81a998
RamonFern <ramonfernandes9@gmail.com>
2019-05-17 16:14:24 -0300

    Mostrando dados e imagem do cliente para página de profile

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { StorageService } from '../../services/storage.service';
+import { ClienteDTO } from '../../models/cliente.dto';
+import { ClienteService } from '../../services/domain/cliente.service';
 
 /**
  * Generated class for the ProfilePage page.
@@ -16,19 +18,36 @@ import { StorageService } from '../../services/storage.service';
 })
 export class ProfilePage {
 
-  email : string;
+  cliente : ClienteDTO;
+
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    public storage: StorageService) {
+    public storage: StorageService,
+    public clienteService: ClienteService) {
   }
 
   ionViewDidLoad() {
    let localUser = this.storage.getLocalUser();
    if(localUser && localUser.email) {
-     this.email = localUser.email;
+     this.clienteService.findByEmail(localUser.email)
+      .subscribe(response => {
+        this.cliente = response;
+        //buscar imagem
+        this.getImageIfExists();
+      }, 
+        error => {});
    }
+  }
+
+  getImageIfExists() {
+    this.clienteService.getImageFromBucket(this.cliente.id)
+    .subscribe(response => {
+      this.cliente.imageUrl = 'https://s3-sa-east-1.amazonaws.com/curso-spring-ionic-rvf/cp'+ this.cliente.id + '.jpg';
+     // this.cliente.imageUrl = '${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg';
+    }, 
+    error => {});
   }
 
 }
---
+++ src/services/domain/cliente.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs/Rx";
+import { ClienteDTO } from "../../models/cliente.dto";
+import { StorageService } from "../storage.service";
+
+@Injectable()
+export class ClienteService {
+
+    constructor(public http: HttpClient, public storage: StorageService){
+
+    }
+
+    findByEmail(email : string) : Observable<ClienteDTO> {
+
+        let token = this.storage.getLocalUser().token;
+        let authHeader = new HttpHeaders({'Authorization':'Bearer '+ token});
+
+                                 
+        return this.http.get<ClienteDTO>(
+            'http://localhost:8080/clientes/email?value='+ email,
+           {'headers': authHeader});/** http://localhost:8080***${API_CONFIG.baseUrl}/.../**${email} */                          
+    }
+
+    getImageFromBucket(id : string) : Observable<any> {
+       // let ur = '${API_CONFIG.bucketBaseUrl}/cp${id}.jpg'
+        let url = 'https://s3-sa-east-1.amazonaws.com/curso-spring-ionic-rvf/cp'+ id + '.jpg'
+        return this.http.get(url, {responseType : 'blob'});
+
+    }
+}
+
+ 

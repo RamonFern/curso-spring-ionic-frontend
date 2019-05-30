ba1b3aab5f3a2b429828dc4cb8f4fe1471bfb99c
RamonFern <ramonfernandes9@gmail.com>
2019-05-29 22:10:55 -0300

    Carregando imagens dos produtos

--- src/services/domain/produto.service.ts
+++ src/services/domain/produto.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
 
 @Injectable()
 export class ProdutoService {
@@ -10,6 +11,13 @@ export class ProdutoService {
     findByCategoria(categoria_id : string) {
         //return this.http.get('${API_CONFIG.baseUrl}/produtos/?categorias=${categoria_id}');
         return this.http.get('http://localhost:8080/produtos/?categorias='+ categoria_id);
+    }
+
+    getSmallImageFromBucket(id : string) : Observable<any> {
+        //let url = '${API_CONFIG.bucketBaseUrl}/prod${id}-small.jpg'
+        let url = 'https://s3-sa-east-1.amazonaws.com/curso-spring-ionic-rvf/prod'+ id + '-small.jpg'
+        return this.http.get(url, {responseType : 'blob'});
+
     }
 
 }
